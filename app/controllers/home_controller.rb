03b0c11aa962526291f79be986c263f42f6c2849
Tommy Willianto <tommywillianto@yahoo.com>
2018-09-08 04:03:19 +0700

    frontend integration

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,23 @@
 class HomeController < ApplicationController
   def index
   end
+
+  helper_method :resource_name, :resource, :devise_mapping, :resource_class
+
+  def resource_name
+    :driver
+  end
+ 
+  def resource
+    @resource ||= Driver.new
+  end
+
+  def resource_class
+    Driver
+  end
+ 
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:driver]
+  end
+
 end
