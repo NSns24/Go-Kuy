7d56ab47fb6edbb3c07909a42efad539b8f803f4
ambrosiusdh <38629013+ambrosiusdh@users.noreply.github.com>
2018-09-07 23:41:04 +0700

    assets added, some not_fix views changed

---
+++ app/assets/javascripts/main_login_page.js
@@ -0,0 +1,39 @@
+var loginAsDriver = $('.login-driver-image');
+var loginAsCustomer = $('.login-goride-image');
+
+loginAsDriver.on('click', loginDriver);
+loginAsCustomer.on('click', loginCustomer);
+
+function loginDriver(){
+
+	loginAsDriver.css('position', 'absolute');
+	loginAsDriver.css('width', '260px');
+	loginAsDriver.css('height', '260px');
+	$('.login-home-content-driver').css('height','80vh');
+	loginAsDriver.animate({
+		"top" : '11%',
+		"right" : '-2.5%',
+		"width" : '464px',
+		"height" : '464px'
+	}, "slow", function(){
+		$('.login-driver').fadeIn();
+	});
+	var driverImage = $('.login-home-content-driver');
+
+}
+
+function loginCustomer(){
+	loginAsCustomer.css('position', 'absolute');
+	loginAsCustomer.css('width', '260px');
+	loginAsCustomer.css('height', '260px');
+	$('.login-home-content-customer').css('height','80vh');
+	loginAsCustomer.animate({
+		"top" : '11%',
+		"left" : '-2.5%',
+		"width" : '464px',
+		"height" : '464px'
+	}, "slow", function(){
+		$('.login-driver').fadeIn();
+	});
+	var driverImage = $('.login-home-content-customer');
+}
